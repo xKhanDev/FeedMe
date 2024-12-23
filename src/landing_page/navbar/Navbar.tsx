d3d9c3cd21ed4e68c9427fead6258fdd8bf87942
xKhanDev <safeerkhanxofficial@gmail.com>
2024-12-23 18:43:48 +0500

    web name changed

--- src/landing_page/navbar/Navbar.tsx
+++ src/landing_page/navbar/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   const navigate = useNavigate();
   return (
     <div className='w-full h-12 md:h-14 lg:h-16 light_bg rounded-sm md:rounded-md lg:rounded-lg flex justify-between items-center px-4 md:px-6 lg:px-8'>
-      <h1 className='capital font-bold text-2xl cursor-pointer' onClick={() => navigate('/')}>FundVerse</h1>
+      <h1 className='capital font-bold text-2xl cursor-pointer' onClick={() => navigate('/')}>FeedMe</h1>
       <button className="my_btn green_bg text-white"
       >Connect</button>
     </div>
